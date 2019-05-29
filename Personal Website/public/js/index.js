926d9e1605a469faa0b27421a2447738cd4ab544
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2019-05-29 14:47:36 +0000

    Add Technologies worked with

--- Personal Website/public/js/index.js
+++ Personal Website/public/js/index.js
@@ -1,8 +1,3 @@
 $('nav a').addClass('hvr-bob');
 $('.card a').addClass('hvr-pulse');
 $('.card').addClass('hvr-float-shadow');
-
-function scrollToAnchor(aid){
-    var aTag = $("a[name='"+ aid +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-}
